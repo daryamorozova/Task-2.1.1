2995c7e434e33729dca8819e908c2f080c284818
Darya Morozova <fitosmd@yandex.ru>
2020-07-11 10:49:18 +0300

    Initial commit

---
+++ src/Miles.java
@@ -0,0 +1,7 @@
+public class Miles {
+    public static void main(String[] args) {
+        long ticket = 25000;
+        long mile = ticket / 20;
+        System.out.println("Bonus miles = " + mile);
+    }
+}
